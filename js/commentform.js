0d36dec764ce4235c5a801a3c91c76274168aaa3
KSPravi <praviks.123@gmail.com>
2026-06-10 22:00:51 +0530

    Add files via upload

--- js/commentform.js
+++ js/commentform.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
-  setTimeout(function () {
-    $("#sucess_msg").fadeOut("fast");
-  }, 1000);
-  // $("#sucess_msg").hide();
   $.validator.addMethod(
     "notplaceholder",
     function (value, element) {
       return $(element).attr("placeholder") != value;
     },
-    "Please enter a value",
+    "Please enter a value"
   );
+  $.validator.addMethod(
+    "phoneNumber",
+    function (value, element) {
+      return this.optional(element) || /^[0-9+\-\s()]{7,20}$/.test(value);
+    },
+    "Please enter a valid phone number"
+  );
+
   $("#comment_form").validate({
     rules: {
       name: {
@@ -24,28 +28,56 @@ $(document).ready(function () {
       },
       phone: {
         required: true,
-        number: true,
+        phoneNumber: true,
+        notplaceholder: true,
+      },
+      message: {
+        required: true,
+        minlength: 10,
         notplaceholder: true,
       },
     },
     messages: {
-      name: "Please enter your First Name",
-
+      name: "Please enter your full name",
       email: {
-        required: "Please enter your Email ",
-        email: "Please enter a valid Email",
+        required: "Please enter your email address",
+        email: "Please enter a valid email address",
       },
-      phone: "Please enter your number",
+      phone: {
+        required: "Please enter your phone number",
+        phoneNumber: "Please enter a valid phone number",
+      },
+      message: {
+        required: "Please enter your message",
+        minlength: "Please enter at least 10 characters",
+      },
     },
     submitHandler: function (form) {
-      $("#sucess_msg").show();
+      var $form = $(form);
+      var $message = $("#sucess_msg");
+      var $button = $form.find('button[type="submit"]');
+      var $buttonText = $button.find("span");
+      var defaultButtonText = $buttonText.text();
+
+      $message.show().html("Sending...");
+      $button.prop("disabled", true);
+      $buttonText.text("Sending...");
+
       $.ajax({
         type: "POST",
-        url: "commentform.php",
-        data: $(form).serialize(), // serializes the form's elements.
-        success: function (data) {
-          $("#sucess_msg").html(data);
-          $("#comment_form").trigger("reset"); // show response from the php script.
+        url: $form.attr("action"),
+        data: $form.serialize(),
+        dataType: "json",
+        success: function () {
+          $message.html("<span>Your message was successfully sent. We will contact you soon.</span>");
+          form.reset();
+        },
+        error: function () {
+          $message.html("<span>Unable to send your enquiry right now. Please try again later.</span>");
+        },
+        complete: function () {
+          $button.prop("disabled", false);
+          $buttonText.text(defaultButtonText);
         },
       });
     },
